9dfa17f342e917fb125c7c4ca1e48839ac1136d5
Stephen Bussey <steve.bussey@salesloft.com>
2018-11-01 15:56:22 -0400

    Remove extra logger

--- lib/push_ex/push/item_server.ex
+++ lib/push_ex/push/item_server.ex
@@ -8,8 +8,6 @@ defmodule PushEx.Push.ItemServer do
     Task.start_link(fn ->
       ms_since_insertion = PushEx.unix_now() - unix_inserted_at
 
-      Logger.debug("Push.ItemServer broadcast channel=#{channel} event=#{event} ms_in_stage=#{ms_since_insertion}")
-
       PushExWeb.PushChannel.broadcast({:msg, channel}, event, data)
       |> case do
         {:ok, :broadcast} ->
