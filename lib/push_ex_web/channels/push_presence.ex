076bb64e5a85c8c8d13688cf6fc543a3ee587f76
Stephen Bussey <steve.bussey@salesloft.com>
2018-11-01 12:23:34 -0400

    Remove the singleton Presence topic
    This can be really intensive to both maintain (chatty protocol) and to list (massive list). Let's
    avoid it unless needed. There are some trade-offs here, which I'm not super happy about, but
    it's better than blowing up the server.

--- lib/push_ex_web/channels/push_presence.ex
+++ lib/push_ex_web/channels/push_presence.ex
@@ -6,14 +6,14 @@ defmodule PushExWeb.PushPresence do
   def track(%Phoenix.Socket{topic: topic} = socket) do
     id = PushEx.Config.presence_identifier_fn().(socket)
 
-    track(socket.channel_pid, "subscribed", id, %{
+    track(socket.channel_pid, id, id, %{
       channel: topic,
       identifier: id,
       online_at: PushEx.unix_now()
     })
   end
 
-  def subscribed() do
-    list("subscribed")
+  def subscribed(id) do
+    list(id)
   end
 end
