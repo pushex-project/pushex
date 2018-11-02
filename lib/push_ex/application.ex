00887d119cbbe8487834be5bc2aa2cbeec2d853d
Stephen Bussey <steve.bussey@salesloft.com>
2018-11-02 11:47:33 -0400

    mix format

--- lib/push_ex/application.ex
+++ lib/push_ex/application.ex
@@ -6,25 +6,29 @@ defmodule PushEx.Application do
   def start(_type, _args) do
     PushEx.Config.check!()
 
-    children = [
-      pre_endpoint_children(),
-      PushExWeb.Endpoint,
-      post_endpoint_children(),
-    ] |> List.flatten()
+    children =
+      [
+        pre_endpoint_children(),
+        PushExWeb.Endpoint,
+        post_endpoint_children()
+      ]
+      |> List.flatten()
 
     opts = [strategy: :one_for_one, name: PushEx.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  def pre_endpoint_children(), do: [
-    PushEx.Push.ItemProducer,
-    PushEx.Push.ItemConsumer,
-    PushEx.Instrumentation.Tracker,
-  ]
+  def pre_endpoint_children(),
+    do: [
+      PushEx.Push.ItemProducer,
+      PushEx.Push.ItemConsumer,
+      PushEx.Instrumentation.Tracker
+    ]
 
-  def post_endpoint_children(), do: [
-    PushExWeb.PushPresence
-  ]
+  def post_endpoint_children(),
+    do: [
+      PushExWeb.PushPresence
+    ]
 
   def config_change(changed, _new, removed) do
     PushExWeb.Endpoint.config_change(changed, removed)
