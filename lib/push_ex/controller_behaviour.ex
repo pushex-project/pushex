08097a0812201c0f6a3c7e13fd6487947810ca37
Stephen Bussey <steve.bussey@salesloft.com>
2018-11-10 01:30:07 -0500

    Document behaviours

--- lib/push_ex/controller_behaviour.ex
+++ lib/push_ex/controller_behaviour.ex
@@ -1,3 +1,7 @@
 defmodule PushEx.ControllerBehaviour do
+  @doc """
+  Accepts a Plug.Conn and params map to determine if the API is allowed to be accessed. It is possible to modify
+  the response Plug.Conn completely, or return simple `:ok | :error` atoms.
+  """
   @callback auth(Plug.Conn.t(), map()) :: :ok | {:ok, Plug.Conn.t(), map()} | :error | {:error, Plug.Conn.t()}
 end
--- lib/push_ex/socket_behaviour.ex
+++ lib/push_ex/socket_behaviour.ex
@@ -1,6 +1,24 @@
 defmodule PushEx.SocketBehaviour do
-  @callback socket_connect(map(), Phoenix.Socket.t()) :: any
-  @callback channel_join(bitstring(), map(), Phoenix.Socket.t()) :: any
-  @callback socket_id(Phoenix.Socket.t()) :: any
-  @callback presence_identifier(Phoenix.Socket.t()) :: any
+  @doc """
+  See https://hexdocs.pm/phoenix/Phoenix.Socket.html#c:connect/3
+  """
+  @callback socket_connect(map(), Phoenix.Socket.t()) :: {:ok, Phoenix.Socket.t()} | :error
+
+  @doc """
+  See https://hexdocs.pm/phoenix/Phoenix.Channel.html#c:join/3
+  """
+  @callback channel_join(bitstring(), map(), Phoenix.Socket.t()) ::
+              {:ok, Phoenix.Socket.t()}
+              | {:ok, reply :: map(), Phoenix.Socket.t()}
+              | {:error, reason :: map()}
+
+  @doc """
+  See https://hexdocs.pm/phoenix/Phoenix.Socket.html#c:id/1
+  """
+  @callback socket_id(Phoenix.Socket.t()) :: bitstring()
+
+  @doc """
+  See https://hexdocs.pm/phoenix/Phoenix.Presence.html#c:track/4 `key`
+  """
+  @callback presence_identifier(Phoenix.Socket.t()) :: bitstring()
 end
