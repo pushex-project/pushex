e257738f71e61158bf37490e115b988189c37cf1
Stephen Bussey <steve.bussey@salesloft.com>
2018-11-10 01:32:33 -0500

    Document Instrumentation behaviour

--- lib/push_ex/instrumentation/push.ex
+++ lib/push_ex/instrumentation/push.ex
@@ -2,9 +2,25 @@ defmodule PushEx.Instrumentation.Push do
   alias PushEx.Push
 
   defmodule Behaviour do
+    @doc """
+    Called when a push is requested to be sent. By default this happens in the API controller.
+    """
     @callback requested(%Push{}) :: any
+
+    @doc """
+    Called when a push is delivered to a Channel. This can occur more, less, or the same number of times as a push
+    being requested.
+    """
     @callback delivered(%Push{}) :: any
+
+    @doc """
+    Called when an API request is started.
+    """
     @callback api_requested() :: any
+
+    @doc """
+    Called immediately before an API response is delivered.
+    """
     @callback api_processed() :: any
   end
 
