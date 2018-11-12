54a28c983b1eed1e1ae7fa9f4e9d7db6bddcbcab
Stephen Bussey <steve.bussey@salesloft.com>
2018-11-12 00:29:06 -0500

    Doc even if not public module

--- lib/push_ex/config.ex
+++ lib/push_ex/config.ex
@@ -1,26 +1,42 @@
 defmodule PushEx.Config do
   @moduledoc false
 
+  @doc """
+  Module to be used for PushEx.Behaviour.Socket implementation
+  """
   def socket_impl() do
     Application.get_env(:push_ex, PushExWeb.PushSocket, [])
     |> Keyword.get(:socket_impl)
   end
 
+  @doc """
+  Module to be used for PushEx.Behaviour.Controller implementation
+  """
   def controller_impl() do
     Application.get_env(:push_ex, PushExWeb.PushController, [])
     |> Keyword.get(:controller_impl)
   end
 
+  @doc """
+  The maximum size of the GenStage item buffer. Increasing this will consume more memory,
+  but will be able to have more items waiting in the case of a serious backup.
+  """
   def producer_max_buffer() do
     Application.get_env(:push_ex, PushExWeb.PushSocket, [])
     |> Keyword.get(:producer_max_buffer, 50_000)
   end
 
+  @doc """
+  Concurrency threshold to take items off of the GenStage item buffer.
+  """
   def producer_max_concurrency() do
     Application.get_env(:push_ex, PushExWeb.PushSocket, [])
     |> Keyword.get(:producer_max_concurrency, 10)
   end
 
+  @doc """
+  The Phoenix.Endpoint implementation to be used.
+  """
   def endpoint() do
     Application.get_env(:push_ex, PushExWeb.PushSocket, [])
     |> Keyword.get(:endpoint, PushExWeb.Endpoint)
