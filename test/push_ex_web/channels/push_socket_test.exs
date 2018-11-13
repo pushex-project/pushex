06d45257b2cada75c4351e7a4c76e6a226e60ce6
Stephen Bussey <steve.bussey@salesloft.com>
2018-11-13 10:41:30 -0500

    Test PushSocket

---
+++ test/push_ex_web/channels/push_socket_test.exs
@@ -0,0 +1,48 @@
+defmodule PushExWeb.PushSocketTest do
+  use PushExWeb.ChannelCase, async: false
+  alias PushExWeb.PushSocket
+
+  import ExUnit.CaptureLog
+
+  describe "connect/2" do
+    test "the connection logic defers to the socket_impl" do
+      PushEx.Test.MockSocket.setup_logging_config()
+
+      log = capture_log(fn ->
+        assert {:ok, socket} = connect(PushSocket, %{})
+      end)
+
+      assert log =~ "Replied PushExWeb.PushSocket :ok"
+      assert log =~ "LoggingSocket socket_connect/2 #{inspect({%{}, "socket"})}"
+      assert log =~ "LoggingSocket socket_id/1 {socket}"
+      assert log =~ "LoggingSocket presence_identifier/1 {socket}"
+      assert String.split(log, "LoggingSocket") |> length() == 4
+    end
+
+    test "the socket is tracked in the global tracker" do
+      PushEx.Test.MockSocket.setup_config()
+
+      assert PushEx.Instrumentation.Tracker.state() == %{
+        channel_pids: %{},
+        transport_pids: %{}
+      }
+
+      capture_log([level: :info], fn ->
+        assert {:ok, socket} = connect(PushSocket, %{})
+        Process.sleep(10)
+
+        transport_pid = socket.transport_pid
+        assert PushEx.Instrumentation.Tracker.state() == %{
+          channel_pids: %{},
+          transport_pids: %{
+            transport_pid => %{
+              identifier: "id",
+              online_at: PushEx.unix_now(),
+              type: :channel_test
+            }
+          }
+        }
+      end)
+    end
+  end
+end
--- test/support/mock_socket.ex
+++ test/support/mock_socket.ex
@@ -5,6 +5,10 @@ defmodule PushEx.Test.MockSocket do
     Application.put_env(:push_ex, PushExWeb.PushSocket, socket_impl: __MODULE__)
   end
 
+  def setup_logging_config do
+    Application.put_env(:push_ex, PushExWeb.PushSocket, socket_impl: PushEx.Test.MockSocket.LoggingSocket)
+  end
+
   def socket_connect(_, socket) do
     {:ok, socket}
   end
@@ -19,5 +23,32 @@ defmodule PushEx.Test.MockSocket do
 
   def presence_identifier(_socket) do
     "id"
+  end
+
+  defmodule LoggingSocket do
+    @behaviour PushEx.Behaviour.Socket
+    require Logger
+
+    def socket_connect(params, socket) do
+      Logger.debug "LoggingSocket socket_connect/2 #{inspect({params, "socket"})}"
+      {:ok, socket}
+    end
+
+    def channel_join(channel, params, socket) do
+      Logger.debug "LoggingSocket channel_join/3 #{inspect({channel, params, "socket"})}"
+      {:ok, socket}
+    end
+
+    def socket_id(_socket) do
+      Logger.debug "LoggingSocket socket_id/1 {socket}"
+
+      "id"
+    end
+
+    def presence_identifier(_socket) do
+      Logger.debug "LoggingSocket presence_identifier/1 {socket}"
+
+      "id"
+    end
   end
 end
