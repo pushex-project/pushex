0f3f328887c225055c499dee0c1f2141dbb72997
Stephen Bussey <steve.bussey@salesloft.com>
2018-11-18 19:47:04 -0500

    Mark load test as shard factor 4

--- examples/load-test/server/config/config.exs
+++ examples/load-test/server/config/config.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :push_ex, PushExWeb.Endpoint,
   url: [host: "localhost"],
   render_errors: [view: PushExWeb.ErrorView, accepts: ~w(json)],
-  pubsub: [name: PushEx.PubSub, adapter: Phoenix.PubSub.PG2],
+  pubsub: [name: PushEx.PubSub, adapter: Phoenix.PubSub.PG2, pool_size: 4],
   http: [port: 4006],
   check_origin: false,
   watchers: [],
