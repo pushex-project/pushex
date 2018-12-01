7691d25805863f752290ae8a432f6c9497a946f5
Steve Bussey <sb8244@cs.ship.edu>
2018-12-01 11:32:44 -0500

    Document the RouterLoader because it’s a macro

--- lib/push_ex_web/router_loader.ex
+++ lib/push_ex_web/router_loader.ex
@@ -1,4 +1,11 @@
 defmodule PushExWeb.RouterLoader do
+  @moduledoc false
+
+  @doc """
+  Loads an AST from config to be used in the Router. This must be an AST because the
+  order of compilation prevents using a module directly (without some sort of hack to change
+  compilation ordering).
+  """
   defmacro __using__([]) do
     with router_config <- Application.get_env(:push_ex, PushExWeb.Router, []),
          ast <- Keyword.get(router_config, :additional_plug_setup) do
