d494a95da33c3db4502629de94e8a8c25c85e16b
Stephen Bussey <sb8244@cs.ship.edu>
2019-04-23 23:31:13 -0400

    Allow both RanchConnectionDrainer for HTTP and HTTPS at the same time

--- lib/push_ex/supervisor.ex
+++ lib/push_ex/supervisor.ex
@@ -1,27 +1,53 @@
 defmodule PushEx.Supervisor do
   use Supervisor
 
+  @shutdown_timeout 10_000
+
   def start_link(opts) do
     Supervisor.start_link(__MODULE__, opts)
   end
 
   def init(_) do
-    children = [
-      PushExWeb.Endpoint,
-      {PushExWeb.PushTracker, [pool_size: PushEx.Application.pool_size()]},
-      {PushEx.Push.Drainer, producer_ref: PushEx.Push.ItemProducer, shutdown: 15_000},
-      {RanchConnectionDrainer, ranch_ref: endpoint_mod(), shutdown: 15_000}
-    ]
+    children =
+      [
+        PushExWeb.Endpoint,
+        {PushExWeb.PushTracker, [pool_size: PushEx.Application.pool_size()]},
+        {PushEx.Push.Drainer, producer_ref: PushEx.Push.ItemProducer, shutdown: @shutdown_timeout}
+      ] ++ ranch_connection_drainers()
 
     opts = [strategy: :one_for_one, name: __MODULE__]
     Supervisor.init(children, opts)
   end
 
-  defp endpoint_mod() do
-    if Application.get_env(:push_ex, PushExWeb.Endpoint) |> Keyword.get(:https) do
-      PushExWeb.Endpoint.HTTPS
+  defp ranch_connection_drainers() do
+    ranch_connection_drainer_http() ++ ranch_connection_drainer_https()
+  end
+
+  defp ranch_connection_drainer_http() do
+    if Application.get_env(:push_ex, PushExWeb.Endpoint) |> Keyword.get(:http) do
+      [
+        %{
+          id: RanchConnectionDrainer.HTTP,
+          start: {RanchConnectionDrainer, :start_link, [PushExWeb.Endpoint.HTTP]},
+          shutdown: @shutdown_timeout
+        }
+      ]
     else
-      PushExWeb.Endpoint.HTTP
+      []
+    end
+  end
+
+  defp ranch_connection_drainer_https() do
+    if Application.get_env(:push_ex, PushExWeb.Endpoint) |> Keyword.get(:https) do
+      [
+        %{
+          id: RanchConnectionDrainer.HTTPS,
+          start: {RanchConnectionDrainer, :start_link, [PushExWeb.Endpoint.HTTPS]},
+          shutdown: @shutdown_timeout
+        }
+      ]
+    else
+      []
     end
   end
 end
