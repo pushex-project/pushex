9d918b9675ac9f773e9fa49209972032c91112b1
Stephen Bussey <sb8244@cs.ship.edu>
2019-05-01 12:49:59 -0400

    Revamp demo for disconnection / unsubscription

--- examples/test_frontend_socket/assets/app.js
+++ examples/test_frontend_socket/assets/app.js
@@ -1,62 +1,58 @@
-(() => {
-  const socket = new Phoenix.Socket("ws://localhost:4004/push_socket", {params: {}})
-  const pushex = new Pushex.Pushex('ws://localhost:4004/push_socket', {})
+// Main demonstration of pushex in normal operation
+const pushex = new Pushex.Pushex('ws://localhost:4004/push_socket', {})
 
-  function joinChannel(name) {
-    const channel = socket.channel(name, {})
-    channel.on("msg", msg => console.log("Got message", name, msg) )
+Notification.requestPermission().then(function(result) {
+  console.log('Notification.requestPermission', result)
+})
 
-    channel.join()
-      .receive("ok", () => console.log("Joined", name) )
-      .receive("error", ({ reason }) => console.log("failed join", name, reason) )
-      .receive("timeout", () => console.log("Networking issue. Still waiting...", name) )
+let stagedNotifications = []
+
+function stageNotification(body) {
+  stagedNotifications.push(body)
+  setTimeout(clearNotifications, 250)
+}
+
+function clearNotifications() {
+  if (stagedNotifications.length > 0) {
+    const body = stagedNotifications.join('\n')
+    stagedNotifications = []
+    spawnNotification(body)
   }
+}
 
-  Notification.requestPermission().then(function(result) {
-    console.log('Notification.requestPermission', result)
-  })
-
-  let stagedNotifications = []
-
-  function stageNotification(body) {
-    stagedNotifications.push(body)
-    setTimeout(clearNotifications, 250)
-  }
-
-  function clearNotifications() {
-    if (stagedNotifications.length > 0) {
-      const body = stagedNotifications.join('\n')
-      stagedNotifications = []
-      spawnNotification(body)
+function spawnNotification(body) {
+    var options = {
+      body: body,
+      icon: '/icon.png'
     }
-  }
+    new Notification('Got data', options)
+}
 
-  function spawnNotification(body) {
-      var options = {
-        body: body,
-        icon: '/icon.png'
-      }
-      new Notification('Got data', options)
-  }
+pushex.subscribe('test').bind('*', (event, data) => {
+  stageNotification(`test: ${event}-${data}`)
+  console.log('test channel received event/data', event, data)
+})
+pushex.subscribe('test2').bind('*', (event, data) => {
+  stageNotification(`test2: ${event}: ${data}`)
+  console.log('test2 channel received event/data', event, data)
+})
+pushex.subscribe('my-public-channel').bind('*', (event, data) => {
+  stageNotification(`test2: ${event}: ${data}`)
+  console.log('test2 channel received event/data', event, data)
+})
 
-  socket.connect()
+pushex.connect()
 
-  joinChannel("test")
-  joinChannel("test2")
-  joinChannel("my-public-channel")
+// Demonstrate unsubscribe from a Channel
+// It stays open but won't receive any messages because the topic is not subscribed to
+// It will disconnect after 5s due to the timeout below
+const pushexTestLeaving = new Pushex.Pushex('ws://localhost:4004/push_socket', {})
+pushexTestLeaving.connect()
+pushexTestLeaving.subscribe('willDisconnect').bind('*', (event, data) => {
+  console.error('Should not have received anything from willDisconnect')
+})
+pushexTestLeaving.unsubscribe('willDisconnect')
 
-  pushex.subscribe('test').bind('*', (event, data) => {
-    stageNotification(`test: ${event}-${data}`)
-    console.log('test channel received event/data', event, data)
-  })
-  pushex.subscribe('test2').bind('*', (event, data) => {
-    stageNotification(`test2: ${event}: ${data}`)
-    console.log('test2 channel received event/data', event, data)
-  })
-  pushex.subscribe('my-public-channel').bind('*', (event, data) => {
-    stageNotification(`test2: ${event}: ${data}`)
-    console.log('test2 channel received event/data', event, data)
-  })
-
-  pushex.connect()
-})()
+setTimeout(() => {
+  pushexTestLeaving.disconnect()
+}, 5000)
